3fc9d8387cd64cedf0f37d9c3084866975a9457e
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-08-01 11:55:34 +0200

    wip: exercice 3. Not complete : lunch break

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //La fonction doit prendre n en paramètre et retourner answer[]
 
-// const FizzBuzz = (n) => {
-//     let answer = [];
+const FizzBuzz = (n) => {
+    let answer = [];
 
-//     for (let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
 
-//         if (i%3===0 && i%5===0) {
-//             answer.push('FizzBuzz');
-//         }
-//         else if (i%3 == 0 && i%5!=0) {
-//         answer.push('Fizz');
-//         }
-//         else if (i%5 == 0 && i%3!=0) {
-//         answer.push('Buzz');
-//         }
-//         else{
-//             answer.push(i.toString())
-//         }
+        if (i % 3 === 0 && i % 5 === 0) {
+            answer.push('FizzBuzz');
+        }
+        else if (i % 3 == 0 && i % 5 != 0) {
+            answer.push('Fizz');
+        }
+        else if (i % 5 == 0 && i % 3 != 0) {
+            answer.push('Buzz');
+        }
+        else {
+            answer.push(i.toString())
+        }
 
-//     }
+    }
 
-//     return answer;
-// }
+    return answer;
+}
 
 // console.log(FizzBuzz(5))
 
@@ -33,19 +33,19 @@
 // Sinon garder le nombre de coté
 // /!\ retourner les postions dans le tableau, pas les nombres eux mêmes.
 
-const twoSum = (numbers,target) => {
+const twoSum = (numbers, target) => {
     const usedNumers = {}; // objet vide pour stocker les nombres déjà utilisés. mieux car clé /valeur !
 
-    for(let i=0; i<numbers.length;i++) { // Parcourir les nombres 
+    for (let i = 0; i < numbers.length; i++) { // Parcourir les nombres 
         const substract = target - numbers[i]; // soustraction pour trouver les nombres qu'il manque pour arriver à target. 
-                                              // En suite on peut chercher le "reste"
-        
-        if (substract in usedNumers){ // si on a déjà vu le nombre, on a trouvé le nombre manquant
-            return [usedNumers[substract],i];
+        // En suite on peut chercher le "reste"
+
+        if (substract in usedNumers) { // si on a déjà vu le nombre, on a trouvé le nombre manquant
+            return [usedNumers[substract], i];
         }
 
         usedNumers[numbers[i]] = i; // sinon je garde le nombre de coté dans les nombres déjà utilisés.
-        
+
     }
 
 }
@@ -54,12 +54,31 @@ const twoSum = (numbers,target) => {
 // const numbers = [2, 7, 11, 15];
 // const target = 9;
 
-const numbers = [3, 2, 4]
-const target = 6
-twoSum(numbers, target);
+// const numbers = [3, 2, 4]
+// const target = 6
+// twoSum(numbers, target);
 
 // const numbers = [3, 3]
 // const target = 6
 // twoSum(numbers, target);
 
-console.log(twoSum(numbers, target))
+// console.log(twoSum(numbers, target));
+
+
+
+
+// function isValid : seulement besoin de tester si on a (), [] ou {}
+
+const isValid = (string) => {
+
+    if (string.includes("()") || string.includes("{}") || string.includes("[]")) {
+    return true;
+  } else {
+    return false;
+  }
+    }
+
+
+const str = '()';
+console.log(isValid(str)); // ça marche ...mais ça fonctionne aussi pour '(){'  :-(
+
